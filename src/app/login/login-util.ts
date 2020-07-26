1220ea82dd1078cdeeb645e29ca5fe66ffb46027
Arun Karikalan <arun.digital.karikalan1994@gmail.com>
2020-07-26 16:10:44 +0530

    missing files added.

---
+++ src/app/login/login-util.ts
@@ -0,0 +1,20 @@
+import jwt_decode from 'jwt-decode';
+
+export const isUserLoggedIn: () => boolean = () => {
+  const authToken = localStorage.getItem('auth_token');
+  if (!authToken) {
+    return false;
+  }
+  let current_time = new Date().getTime() / 1000;
+  let exp = jwt_decode(authToken).exp;
+  if (!exp) {
+    return true;
+  }
+  if (current_time > exp) {
+    return false;
+  }
+
+  return true;
+};
+
+export const isUserNotLoggedIn: () => boolean = () => !isUserLoggedIn();
